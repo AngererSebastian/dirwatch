5e6b9b196948eb65ca33d98387090e2e3a71c9f7
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2022-07-11 12:43:26 +0200

    implement dirwatching based solely on filenames

---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE NumericUnderscores #-}
+module Main where
+
+import System.Directory (listDirectory)
+import System.Environment (getArgs)
+import Control.Concurrent (threadDelay)
+import Control.Monad (mapM_, (>>))
+import Data.List ((\\))
+
+go :: FilePath -> [FilePath] -> IO ()
+go dir fs = do
+    new <- listDirectory dir
+
+    mapM_ putStrLn $ new \\ fs
+
+    threadDelay 5_000_000 -- wait 5 seconds
+    go dir new
+
+main :: IO ()
+main = do
+    (dir: _) <- getArgs
+    paths <- listDirectory dir
+    go dir paths
